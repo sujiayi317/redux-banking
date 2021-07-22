580caae4e3608ee5a401f5acfe88b2c37e478a1c
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-21 23:21:29 -0400

    Create a store and basic reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {createStore} from 'redux'
+import {bankingReducer} from './reducers/bankingReducers'
+
+const store = createStore(bankingReducer)
+
+
 
 ReactDOM.render(<App/>, document.getElementById("root"))
